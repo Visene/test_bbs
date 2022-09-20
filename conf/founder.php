c1dc9c0401847a6fadea8532b5e98ded9bf0137b
test <test@example.com>
2022-09-20 18:23:52 +0800

    fix err

---
+++ conf/founder.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @author lindong <lindong@> 2015-11-2
+ * @link http://-:80
+ * @license
+ */
+  return array(
+	'admin' => '21232f297a57a5a743894a0e4a801fc3',
+);
+?>
---
+++ git.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:788ad8838fbb6120deca15dbc6783720abf79ce1524eff3f7fb9eb828ee2de00
+size 256
