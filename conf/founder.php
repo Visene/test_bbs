ad9231849f3fb017dcdb233128aaf0e04619c0c5
test <test@example.com>
2023-05-05 17:15:14 +0800

    fix err

---
+++ conf/founder.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @author lindong <lindong@.m> 2015-11-2
+ * @link http://www.baidu.com:
+ * @license
+ */
+  return array(
+	'admin' => '123',
+);
+?>
---
+++ git.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68c087cef2fef36959a94fce9f6d55cf334f632354e44b1b1fa095df5031c924
+size 257
